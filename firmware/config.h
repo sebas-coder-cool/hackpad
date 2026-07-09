00c50f957ff21f625cddd86bfa04b61c78dd31aa
sebas-coder-cool <sebasveenstra5@gmail.com>
2026-07-09 18:50:21 +0200

    Add files via upload

---
+++ firmware/config.h
@@ -0,0 +1,4 @@
+#pragma once
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
+#define OLED_DISPLAY_128X32
---
+++ firmware/keymap.c
@@ -0,0 +1,28 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ 7 │ 8 │ 9 │
+     * ├───┼───┼───┤
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┤
+     * │ 1 │ 2 │ 3 │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_7,   KC_8,   KC_9,
+        KC_4,   KC_5,   KC_6,
+        KC_1,   KC_2,   KC_3
+    )
+};
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    oled_write_P(PSTR("My awesome macropad!\n"), false);
+    return false;
+}
+#endif
